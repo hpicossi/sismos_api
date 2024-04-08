a18a1188e00682b3e7805594c0e7d568afbd3d32
Hilario Iñigo Menendez Picossi <hpicossi@gmail.com>
2024-04-07 22:57:02 -0300

    agregando los controladores

--- app/controllers/features_controller.rb
+++ app/controllers/features_controller.rb
@@ -1,7 +1,21 @@
 class FeaturesController < ApplicationController
   def index
+    @features = Feature.all
+    render json: @features
   end
 
   def create
+    @feature = Feature.new(feature_params)
+    if @feature.save
+      render json: @feature, status: :created
+    else
+      render json: @feature.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def feature_params
+    params.require(:feature).permit(:external_id, :magnitude, :place, :time, :tsunami, :mag_type, :title, :longitude, :latitude)
   end
 end
